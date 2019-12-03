2cffc0dd56dbd595834e31c28a3843d91c452824
Ciprian Caba <ciprian.caba@adswizz.com>
2019-12-03 13:01:25 +0200

    First commit

---
+++ PluginA/PluginA/PluginA.h
@@ -0,0 +1,19 @@
+//
+//  PluginA.h
+//  PluginA
+//
+//  Created by Ciprian Caba on 12/3/19.
+//  Copyright © 2019 AdsWizz, inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PluginA.
+FOUNDATION_EXPORT double PluginAVersionNumber;
+
+//! Project version string for PluginA.
+FOUNDATION_EXPORT const unsigned char PluginAVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PluginA/PublicHeader.h>
+
+
